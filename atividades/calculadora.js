0164d0c9cc9f3fea195a3487daba723560c2dbc4
ERIKVL-dev <erikdovale15@gmail.com>
2026-03-22 15:16:04 -0700

    Add files via upload

---
+++ atividades/calculadora.js
@@ -0,0 +1,50 @@
+console.log("NOVA CALCULADORA");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("QUAL O PRIMEIRO NÚMERO QUE VOCÊ DESEJA?: ", function(num1) {
+
+  num1 = Number(num1);
+
+  rl.question("QUAL O SEGUNDO NÚMERO QUE VOCÊ DESEJA?: ", function(num2) {
+
+    num2 = Number(num2);
+
+    rl.question("QUAL A OPERAÇÃO DESEJADA? (+, -, *, /): ", function(operacao) {
+
+
+      if (operacao === "+") {
+
+        console.log ('resultado: '(num1 + num2));
+
+      } else if (operacao === "-") {
+
+        console.log ('resultado: '(num1 - num2));
+
+      } else if (operacao === "*") {
+
+        console.log ('resultado: '(num1 * num2));
+
+      } else if (operacao === "/") {
+
+        console.log ('resultado: '(num1 / num2));
+
+      } else {
+        console.log("Operação inválida!");
+        rl.close();
+        return;
+      }
+
+      console.log("Resultado:", resultado);
+      rl.close();
+
+    });
+
+  });
+
+});
