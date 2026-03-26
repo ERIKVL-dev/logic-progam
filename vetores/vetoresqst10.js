3bb1ba6d12a60c2ceff4e06393365d53b7bea958
ERIK-dev <erikdovale15@gmail.com>
2026-03-25 21:06:19 -0300

    vetoresqst10

---
+++ vetores/vetoresqst10.js
@@ -0,0 +1,5 @@
+let names = ["Ana", "Bruno", "Carlos", "Eva"]
+
+let nomes = names.filter( names => names.length > 4 )
+
+console.log (nomes)
---
+++ vetores/vetoresqst9.js
@@ -0,0 +1,5 @@
+let num = [5, 12, 8, 20, 3]
+
+let numeros = num.filter( num => num >= 10)
+
+console.log (numeros)
