208289aacb6cf285f5824518f5de041fe088ecc8
ERIK-dev <123123m>
2026-04-10 20:03:48 -0300

    att

--- sistemas/sistema01israel.js
+++ sistemas/sistema01israel.js
@@ -3,6 +3,7 @@ const prompt = require('prompt-sync')();
 const produto = [];
 const prazo = [];
 const quantidade = [];
+const setor = [];
 let opcao = 0;
 
 while (opcao !== 5) {
@@ -18,48 +19,52 @@ while (opcao !== 5) {
 // CADASTRO //
 
     if (opcao == 1) {
-        let produto2 = prompt("QUAL PRODUTO O SENHOR(a) DESEJA?: ");
-        let quantidade2 = Number(prompt("QUANTIDADE DESEJADA?: "));
-        let prazo2 = Number(prompt("QUANTOS DIAS PARA ENTREGAR?: "));
+        let setor2 = prompt('Qual o setor do produto desejado?');
+        let produto2 = prompt("Qual produto o senhor(a) deseja?: ");
+        let quantidade2 = Number(prompt("quantidade desejada?: "));
+        let prazo2 = Number(prompt("Quantos dias para entregar?: "));
 
         produto.push(produto2);
         quantidade.push(quantidade2);
         prazo.push(prazo2);
+        setor.push(setor2);
     }
-
-
 
 
 // LISTA DE PEDIDOS //
 
-    else if (opcao == 2) {
+    if (opcao == 2) {
 
         console.log('\n--- LISTA DE PEDIDOS ---');
 
         for (let i = 0; i < produto.length; i++) {
-         console.log(`PEDIDO ${i + 1}`);
-         console.log(`PRODUTO: ${produto[i]}`);
-         console.log(`QUANTIDADE: ${quantidade[i]}`);
-         console.log(`PRAZO DE ENTREGA: ${prazo[i]} dias`);
+         console.log(`Pedido: ${i + 1}`);
+         console.log(`Produto: ${produto[i]}`);
+         console.log(`Quantidade: ${quantidade[i]}`);
+         console.log(`Prazo de entrega: ${prazo[i]} dias`);
 
-            if (prazo.length <= 2) {
-                console.log('PRIORIDADE: URGENTE!!');
+             if (prazo[i] <= 2) {
+                console.log('Prioridade: Urgente!!');
             } 
-            else if (prazo.length >= 3 && prazo.length <= 5) {
-                console.log('PRIORIDADE: ALTA!');
+             if (prazo[i] >= 3 && prazo[i] <= 5) {
+                console.log('Prioridade: Alta!');
             } 
-            else if (prazo.length >= 6 && prazo.length <= 10) {
-                console.log('PRIORIDADE: MÉDIA');
+             if (prazo[i] >= 6 && prazo[i] <= 10) {
+                console.log('Prioridade: Média');
             } 
-            else if (prazo.length > 10) {
-                console.log('PRIORIDADE: BAIXA');
+             if (prazo[i] > 10) {
+                console.log('Prioridade: Baixa');
             }
-            console.log('----------------------');
+            else {
+                console.log('Nenhum prazo digitado!!')
+            }
+            console.log('-----------------------');
         }
     }
 
-    else if (opcao == 3){
-
+     if (opcao == 3){
+        // mostrar  maior pdd, qntt de pedd , setor top, itens, e urgente
+        console.log(`Maior pedido: ${produto[i]} \n tendo a quantidade pedida${quantidade[i].max} `)
     }
 
 // SAIR DO SISTEMA // 
